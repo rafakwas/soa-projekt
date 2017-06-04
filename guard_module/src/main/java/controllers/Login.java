5d21939a500707d7a231af3746b784ff40c3c7e3
rafakwas <ralphigi@gmail.com>
2017-06-04 18:30:58 +0200

    welcome site and login site added to guard module

---
+++ guard_module/src/main/java/controllers/Login.java
@@ -0,0 +1,73 @@
+package controllers;
+
+import utils.SessionUtils;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+
+
+@ManagedBean
+@SessionScoped
+public class Login implements Serializable {
+
+    private static final long serialVersionUID = 1094801825228386363L;
+
+    private String pwd;
+    private String msg;
+    private String user;
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    //validate login
+    public String validateUsernamePassword() {
+        boolean valid = checkCredentials(user, pwd);
+        if (valid) {
+            HttpSession session = SessionUtils.getSession();
+            session.setAttribute("username", user);
+            return "guard";
+        } else {
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Incorrect Username and Passowrd",
+                            "Please enter correct username and Password"));
+            return "login";
+        }
+    }
+    //logout event, invalidate session
+    public String logout() {
+        HttpSession session = SessionUtils.getSession();
+        session.invalidate();
+        return "login";
+    }
+
+    private boolean checkCredentials(String user, String pwd) {
+        return (user.equals(pwd)) ? true : false;
+    }
+}
