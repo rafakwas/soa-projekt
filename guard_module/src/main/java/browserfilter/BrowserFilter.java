fd4cd377d0bc1078c6fff9c03d62855522710e50
rafakwas <ralphigi@gmail.com>
2017-06-08 17:57:38 +0200

    working with new database. remember about adding md5 and base64 when security domain is deleted

---
+++ guard_module/src/main/java/browserfilter/BrowserFilter.java
@@ -0,0 +1,25 @@
+//package browserfilter;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//import java.util.logging.Logger;
+//
+//@WebFilter(filterName = "BrowserFilter", urlPatterns = { "*.xhtml" })
+//public class BrowserFilter implements Filter {
+//    private final static Logger LOGGER = Logger.getLogger(BrowserFilter.class.toString());
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {}
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest req = (HttpServletRequest) servletRequest;
+//        String userAgent = req.getHeader("User-Agent");
+//        LOGGER.info("Client user agent: " + userAgent);
+//    }
+//
+//    @Override
+//    public void destroy() {}
+//}
